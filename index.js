d1ddd9e7d8d4270b2f774fffa0783abb6d884d51
johnlindquist <johnlindquist@gmail.com>
2015-12-06 22:56:24 -0700

    updating to 1000

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = function(){
     var faker = require("faker");
     var _ = require("lodash");
     return {
-        people: _.times(100, function (n) {
+        people: _.times(1000, function (n) {
             return {
                 id: n,
                 name: {
