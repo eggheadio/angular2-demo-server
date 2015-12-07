e2848d06bd0a2f0232013c4faea8ec139575bd28
johnlindquist <johnlindquist@gmail.com>
2015-12-06 22:54:27 -0700

    server ready

---
+++ index.js
@@ -0,0 +1,26 @@
+module.exports = function(){
+    var faker = require("faker");
+    var _ = require("lodash");
+    return {
+        people: _.times(100, function (n) {
+            return {
+                id: n,
+                name: {
+                  first: faker.name.firstName(),  
+                  last: faker.name.lastName()  
+                },
+                email: faker.internet.email(),
+                username: faker.internet.userName(),
+                avatar: faker.internet.avatar(),
+                company: faker.company.companyName(),
+                address: {
+                    streetAddress: faker.address.streetAddress(),
+                    city: faker.address.city(),
+                    state: faker.address.state(),
+                    zipCode: faker.address.zipCode(),
+                },
+                phone: faker.phone.phoneNumber()
+            }
+        })
+    }
+}
